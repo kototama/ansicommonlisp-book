8c40921d480fa4cda3056421508aeccac88c6dcc
kototama <kototamo+code@gmail.com>
2009-09-18 22:41:44 +0200

    Removed uncessary quote

--- ch02/ex3.lisp
+++ ch02/ex3.lisp
@@ -6,6 +6,6 @@
 (our-fourth '(1 2 3)) ;; nil
 (our-fourth '(1 2 3 4)) ;; 4
 (our-fourth '(a nil c d e f)) ;; d
-(our-fourth '(a b c '(aa bb) d)) ;; (AA BB)
+(our-fourth '(a b c (aa bb) d)) ;; (AA BB)
 
 
