ffccffc77f708453d7ffa2f187aaf0ed48b96e11
kototama <kototamo+code@gmail.com>
2010-02-03 15:46:44 +0100

    return nil for the second value if the list has only one value

--- ch06/ex4.lisp
+++ ch06/ex4.lisp
@@ -2,8 +2,8 @@
   (if (null lst)
       (values nil nil)
       (let* ((max (funcall fn (car lst)))
-             (max2 max))
-        (dolist (obj (cdr lst))
+             (max2 (funcall fn (cadr lst))))
+        (dolist (obj (cddr lst))
           (let ((score (funcall fn obj)))
             (if (> score max)
                 (setf max2 max
@@ -16,3 +16,24 @@
 (most #'length '((a b) (a b c) (a b c d)))
 ;; 4
 ;; 3
+
+(most #'identity '(-3))
+;; -3
+;; NIL
+
+(most #'identity '(6 5))
+;; 6
+;; 5
+
+(most #'identity '(-41 -42))
+;; -41
+;; -42
+
+
+(most #'identity '(1 2 3 4 5 6))
+;; 6
+;; 5
+
+(most #'identity '(-1 -2 -3 -4 -5 -6))
+;; -1
+;; -2
